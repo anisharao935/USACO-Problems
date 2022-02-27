e80f115703535aafa455077482bb2fa2006d3397
anisharao935 <99936383+anisharao935@users.noreply.github.com>
2022-02-26 23:01:29 -0800

    Update painttime.java

--- painttime.java
+++ painttime.java
@@ -1,3 +1,6 @@
+/* 2021 January Contest, Silver Problem #2: No Time to Paint
+http://usaco.org/index.php?page=viewproblem2&cpid=1087
+topics: prefix/suffix sums */
 import java.io.*;
 import java.util.*;
 public class painttime {
@@ -12,36 +15,22 @@ public class painttime {
         int[] fence = new int[N];
         for (int i=0;i<N;i++) { // convert fence to number code
             fence[i] = (int) inputFence.charAt(i) - (int) ('A'); }
-        //System.out.println(Arrays.toString(fence));
         // calculate prefix and suffix sums
         int[] prefix_sums = calculate_sums(fence);
-        //System.out.println(Arrays.toString(prefix_sums));
         int[] suffix_sums = reverse(calculate_sums(reverse(fence)));
-        //System.out.println(Arrays.toString(suffix_sums));
         // answer queries
         PrintWriter out = new PrintWriter(System.out);
         for (int q=0;q<Q;q++) {
             st = new StringTokenizer(input.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            //System.out.println(a + " " + b);
-            //System.out.println(prefix_sums[a-1] + " " + suffix_sums[b]);
             int ans = 0;
             if (a>1) ans += prefix_sums[a-1];
             if (b<N) ans += suffix_sums[b];
             out.println(ans); }
         out.close();
     }
-    static int[] reverse(int[] arr) {
-        Integer[] newArr = new Integer[arr.length];
-        Arrays.setAll(newArr, i->arr[i]);
-        List<Integer> arrList = Arrays.asList(newArr);
-        Collections.reverse(arrList);
-        Object[] reversedArr = arrList.toArray();
-        int[] outputArr = new int[arr.length];
-        Arrays.setAll(outputArr, i->(int)reversedArr[i]);
-        return outputArr;
-    }
+    
     static int[] calculate_sums(int[] input) {
         // calculates the prefix sums for a given input fence
         int[] prefix_sums = new int[input.length+1];
@@ -49,7 +38,6 @@ public class painttime {
         int[] last_appeared = new int[26];
         Arrays.fill(last_appeared, -1);
         for (int i=1;i<=input.length;i++) {
-            //System.out.println(Arrays.toString(prefix_sums));
             prefix_sums[i] = prefix_sums[i-1];
             int color = input[i-1];
             if (last_appeared[color] == -1) {
@@ -62,5 +50,17 @@ public class painttime {
             }
             last_appeared[color] = i; }
         return prefix_sums;
+    }
+    
+    static int[] reverse(int[] arr) {
+        // reverses an input integer array
+        Integer[] newArr = new Integer[arr.length];
+        Arrays.setAll(newArr, i->arr[i]);
+        List<Integer> arrList = Arrays.asList(newArr);
+        Collections.reverse(arrList);
+        Object[] reversedArr = arrList.toArray();
+        int[] outputArr = new int[arr.length];
+        Arrays.setAll(outputArr, i->(int)reversedArr[i]);
+        return outputArr;
     }
 }
